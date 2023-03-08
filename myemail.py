c207f5a9a703f0f3da38bfbbdd5391cebec4f454
Kejrp23 <j.pittman51@att.net>
2023-03-08 13:02:48 -0500

    added boilerplate for emails

--- myemail.py
+++ myemail.py
@@ -1,4 +1,20 @@
 from smtplib import SMTP
 
 
+server = smtplib.SMTP("smtp.gmail.com", port=587)
+
+
+sender = 'NoReply@gmail.com'
+
+to = 'unknown@yahoo.com'
+
+username =''
+password = ''
+
+
+
+
+server.starttls()
+server.sendmail(sender, to, "message" )
+
 
