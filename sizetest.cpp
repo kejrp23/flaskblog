59a665468753bc21bef8a74c56cf43f982ac702d
Kejrp23 <j.pittman51@att.net>
2023-04-13 22:38:48 -0400

    updated with c++ size of bytes and files test work

---
+++ sizetest.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+
+int main(){
+		ifstream file {"blogquery.cpp"};
+		vector <string> contents;	
+		cout << sizeof(file);
+		string line;
+		getline(cin,line){
+				line.push_back(contents);
+		}
+		for (words:contents){
+				cout << words << endl;
+		}
+		return 0;
+
+}
