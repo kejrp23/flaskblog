c558f59f2064e643812a0af3abec461d21b743c5
Kejrp23 <j.pittman51@att.net>
2023-05-18 13:38:04 -0400

    updated insert method - working copy of program

--- blogprog2.cpp
+++ blogprog2.cpp
@@ -2,7 +2,14 @@
 #include "sqlite3.h"
 
 
-using namespace std;
+void clear(){
+		try{
+				system("cls");
+		}
+		catch(...){
+				system("clear");
+		}
+}
 
 void insert(){
 		//initializing variables to construct entry	
@@ -11,21 +18,21 @@ void insert(){
 	//	std::string author;
 	//	std::string subject;
 		//char message[2000];
-		char title[20];
+		std::string title;
 		char author[30];
-		char subject[20];
+//		char subject[20];
 		char post[3000];
 
 	//Creating entry	
 		std::cout << "Please Enter a title: ";
-		std::cin >> title;
+		getline(std::cin,title,';');
 		std::cout << "Please begin typing, please add a ';' to the end when you are done\n >";
 		getline(std::cin,message,';');
 		std::cout << "Enter Author name: ";
 		std::cin >> author;	
-		std::cout << "Subject: ";
-		std::cin >> subject;	
-		sprintf(post,"Insert into BlogEntries Values('%s','%s','%s','%s')",title,message.c_str(),author,subject);
+//		std::cout << "Subject: ";
+//		std::cin >> subject;	
+		sprintf(post,"Insert into BlogEntries Values('%s','%s','%s','%s')",title.c_str(),message.c_str(),author,"C++");
 		std::cout << post << std::endl;
 }
 
@@ -33,9 +40,9 @@ void insert(){
 static int callback(void *NotUsed, int count, char **value, char **column) {
    int i;
    for(i = 0; i< count; i++) {
-		   cout << column[i]<< " : " << value[i] << endl;
+		   std::cout << column[i]<< " : " << value[i] << std::endl;
    }
-   cout << endl;
+   std::cout << std::endl;
    return 0;
 }
 
@@ -51,32 +58,38 @@ static int sql_insert(){
 	//	std::string author;
 	//	std::string subject;
 		//char message[2000];
-		char title[20];
-		char author[30];
-		char subject[20];
-		char post[3000];
+		char title[50];
+		char author[50];
+		char subject[50];
+		char post[5000];
+		char date[15];
 
 	//Creating entry	
+		std::cout << "Please Enter todays date in YYYY-MM-DD Format\n>";
+		std::cin >> date;
 		std::cout << "Please Enter a title: ";
 		std::cin >> title;
-		std::cout << "Please begin typing, please add a ';' to the end when you are done\n >";
+		std::cout << "Please begin typing, please add a ';' to the end when you are done\n>";
 		getline(std::cin,message,';');
 		std::cout << "Enter Author name: ";
 		std::cin >> author;	
 		std::cout << "Subject: ";
 		std::cin >> subject;	
-		sprintf(post,"Insert into BlogEntries Values('%s','%s','%s','%s')",title,message.c_str(),author,subject);
-		//std::cout << post << std::endl;
+		sprintf(post,"Insert into BlogEntries(Date,Title,Post,Author,Subject) Values('%s','%s','%s','%s','%s');",date,title,message.c_str(),author,subject);
+//		std::cout << post << std::endl;
 		
 		
-		response = sqlite3_exec(db,post.c_str(),NULL,0,NULL);
+		response = sqlite3_exec(db,post,NULL,0,NULL);
 		if (response != SQLITE_OK){
-				cerr << "Error with insert" << endl;
+				std::cerr << "Error with insert\n";
 		}
 		else{
-				cout << "Data inserted successfully" << endl;
+				std::cout << "Data inserted successfully\n";
 		};
-	return 0;
+		
+		clear();	
+	
+		return 0;
 };
 
 
@@ -84,29 +97,21 @@ static int sql_query(){
 
 		sqlite3* db;
 		int response;
-		string sql("select * from BlogEntries");
+		std::string sql("select * from BlogEntries");
 		response = sqlite3_open("./db/blogs.db", &db);
 		
 		response = sqlite3_exec(db,sql.c_str(),callback,NULL,NULL);
 		if (response != SQLITE_OK){
-				cerr << "Error"<<endl;
+				std::cerr << "Error\n";
 		}
 		return 0;
 }
 
-void clear(){
-		try{
-				system("cls");
-		}
-		catch(...){
-				system("clear");
-		}
-}
 
 
 void start(){
 		int action;				
-		cin >> action;	
+		std::cin >> action;	
 		if (action == 1){
 			sql_insert();
 		//	insert();
@@ -118,7 +123,7 @@ void start(){
 //				quit();
 		}
 		else {
-				cout << "entry not allowed\n";
+				std::cout << "entry not allowed\n";
 				start();
 		}
 }
@@ -130,16 +135,16 @@ int main(){
 		int response;
 		response = sqlite3_open("./db/blogs.db",&db);
 		if(response != SQLITE_OK){
-				cerr << "Error occurred: File not opened"<< endl;
+				std::cerr << "Error occurred: File not opened\n";
 		}
 		else {
 				clear();
-				cout << "File Opened successfully \n";
+				std::cout << "File Opened successfully \n";
 		}
-		cout << "**** Welcome to the Blog Editor ****\n\n" << endl;
-		cout << "What Would you like to do?\n" << endl;
-		cout << "1. Add entry \n2. Query Database\n3. Quit (work in progress)\n";
-		cout << ">";
+		std::cout << "**** Welcome to the Blog Editor ****\n\n";
+		std::cout << "What Would you like to do?\n";
+		std::cout << "1. Add entry \n2. Query Database\n3. Quit (work in progress)\n";
+		std::cout << ">";
 		start();
 		return 0;
 }
